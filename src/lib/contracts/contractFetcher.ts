f7ba10a9d8d2ea4f50f4e16b73efe92067173e5b
Admin <18885151188@139.com>
2023-11-15 10:10:49 +0800

    注释。。

--- src/lib/contracts/contractFetcher.ts
+++ src/lib/contracts/contractFetcher.ts
@@ -15,6 +15,7 @@ export const contractFetcher = <T>(library: Web3Provider | undefined, contractIn
 
     const method = ethers.utils.isAddress(arg0) ? arg1 : arg0;
 
+    //传入合约调用的参数，返回合约调用的promise
     const contractCall = getContractCall({
       provider,
       contractInfo,
@@ -60,7 +61,38 @@ export const contractFetcher = <T>(library: Web3Provider | undefined, contractIn
           reject(e);
         });
     };
+   // Promise构造函数接受一个函数作为参数，该函数的两个参数分别是resolve和reject。它们是两个函数，由 JavaScript 引擎提供，不用自己部署。
+   // 分别表示 Promise 成功和失败的状态。起始函数执行成功时，它应该调用 resolve 函数并传递成功的结果。当起始函数执行失败时，它应该调用 reject 函数并传递失败的原因。
+   // Promise 构造函数返回一个 Promise 对象，该对象具有以下几个方法：
+   // then：用于处理 Promise 成功状态的回调函数。
+   // catch：用于处理 Promise 失败状态的回调函数。
+   // finally：无论 Promise 是成功还是失败，都会执行的回调函数。
+   //  const promise = new Promise((resolve, reject) => {
+   //    // 异步操作
+   //    setTimeout(() => {
+   //      if (Math.random() < 0.5) {
+   //        resolve('success');
+   //      } else {
+   //        reject('error');
+   //      }
+   //    }, 1000);
+   //  });
+   //
+   //  promise.then(result => {
+   //    console.log(result);
+   //  }).catch(error => {
+   //    console.log(error);
+   //  });
 
+    // async函数返回一个 Promise 对象。
+    // async函数内部return语句返回的值，会成为then方法回调函数的参数。
+    // async function f() {
+    //   return 'hello world';
+    // }
+    //
+    // f().then(v => console.log(v))
+    // // "hello world"
+    // async函数完全可以看作多个异步操作，包装成的一个 Promise 对象，而await命令就是内部then命令的语法糖。
     return new Promise(async (resolve, reject) => {
       contractCall
         .then((result) => {
@@ -79,6 +111,7 @@ export const contractFetcher = <T>(library: Web3Provider | undefined, contractIn
     });
   };
 
+//解构传参数
 function getContractCall({ provider, contractInfo, arg0, arg1, method, params, additionalArgs }) {
   if (ethers.utils.isAddress(arg0)) {
     const address = arg0;
